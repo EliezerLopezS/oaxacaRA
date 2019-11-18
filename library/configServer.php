0fa0dd83ee7e451c4b1cc08dca0c8f22c7b2870f
EliezerLopezS <55566368+EliezerLopezS@users.noreply.github.com>
2019-11-18 17:25:18 -0600

    modificando archivo de conexion

--- library/configServer.php
+++ library/configServer.php
@@ -1,5 +1,5 @@
 <?php
-define("USER", "root");
-define("SERVER", "localhost");
-define("BD", "oaxaca");
-define("PASS", "");
+define("USER", "bd9ccbbf3734de");
+define("SERVER", "us-cdbr-iron-east-05.cleardb.net");
+define("BD", "heroku_8fe5793f88612b3");
+define("PASS", "e23e34bb");
